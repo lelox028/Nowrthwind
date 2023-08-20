455cc9c4de7d2432a53627a0ee79fe265b3aa033
Vraxh <barrosownicolas@gmail.com>
2023-08-20 20:08:21 -0300

    Nota en main, para correcciones

--- Northwind/src/main/java/org/example/Main.java
+++ Northwind/src/main/java/org/example/Main.java
@@ -2,5 +2,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.print("---");
+        // IMPORTANTE
+        // Restante por hacer
+        // --> Herencia, para ya conectar la Data Base
+        // --> Hay variables de intacias de clases que faltan
+        // --> Hacer el Fix general y una vez este el fix empezar con las otras ramas
+        // --> Queda revisar bien el codigo, por si falta alguna variable esta mal y listo
+        
     }
 }
