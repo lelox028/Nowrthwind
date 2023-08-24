f6c9e920acb4505ed914e7059a8cad48611e4c0f
Vraxh <barrosownicolas@gmail.com>
2023-08-23 21:34:04 -0300

    constructores sin uso

--- Northwind/src/main/java/org/example/Main.java
+++ Northwind/src/main/java/org/example/Main.java
@@ -1,14 +1,6 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        System.out.print("---");
-        Categories c = new Categories();
-        Customers cus = new Customers();
-        Employees em = new Employees();
-        OrderDetails od = new OrderDetails();
-        Orders or = new Orders();
-        Products p = new Products();
-        Shippers s = new Shippers();
-        Suppliers sup = new Suppliers();
+        
     }
 }
